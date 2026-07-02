bf2a11267570cfc875bc58fdfc72e6a0b91cfca8
myjeelewacje <kontakt@myjeelewacje.pl>
2026-07-02 19:25:32 +0100

    Fix Payload layout server function
    Refactor layout component to include server function handling.

--- src/app/(payload)/layout.tsx
+++ src/app/(payload)/layout.tsx
@@ -1,9 +1,33 @@
-import type { ReactNode } from 'react'
 import config from '@payload-config'
-import { RootLayout } from '@payloadcms/next/layouts'
-import { importMap } from './admin/importMap.js'
-import '../globals.css'
+import '@payloadcms/next/css'
+import type { ServerFunctionClient } from 'payload'
+import { handleServerFunctions, RootLayout } from '@payloadcms/next/layouts'
+import React from 'react'
 
-export default ({ children }: { children: ReactNode }) => (
-  <RootLayout config={config} importMap={importMap}>{children}</RootLayout>
+import { importMap } from './admin/importMap.js'
+
+type Args = {
+  children: React.ReactNode
+}
+
+const serverFunction: ServerFunctionClient = async function (args) {
+  'use server'
+
+  return handleServerFunctions({
+    ...args,
+    config,
+    importMap,
+  })
+}
+
+const Layout = ({ children }: Args) => (
+  <RootLayout
+    config={config}
+    importMap={importMap}
+    serverFunction={serverFunction}
+  >
+    {children}
+  </RootLayout>
 )
+
+export default Layout
