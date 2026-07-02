3a78a605f8d4feb66cee6f390279d7b4f177550c
myjeelewacje <kontakt@myjeelewacje.pl>
2026-07-02 23:11:37 +0100

    Simplify RootLayout component usage

--- src/app/(payload)/layout.tsx
+++ src/app/(payload)/layout.tsx
@@ -21,11 +21,7 @@ const serverFunction: ServerFunctionClient = async function (args) {
 }
 
 const Layout = ({ children }: Args) => (
-  <RootLayout
-    config={config}
-    importMap={importMap}
-    serverFunction={serverFunction}
-  >
+  <RootLayout config={config} importMap={importMap} serverFunction={serverFunction}>
     {children}
   </RootLayout>
 )
